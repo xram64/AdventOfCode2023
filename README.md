5a2733f7db9ffb355831aa95283adb1e325cd073
xram64 <33797815+xram64@users.noreply.github.com>
2023-12-01 21:03:06 -0500

    Day 1, Part 2

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 ```
 [Part 1] Sum of calibration values: 55002
 ```
+
+### Part 2
+```
+[Part 2] Sum of corrected calibration values: 55093
+```
--- day01/day01.py
+++ day01/day01.py
@@ -8,7 +8,7 @@
 #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   #
 #             __.-----------.___________________________             #
 #            |  |  Answers  |   Part 1: 55002           |            #
-#            |  `-----------'   Part 2:                 |            #
+#            |  `-----------'   Part 2: 55093           |            #
 #            `------------------------------------------'            #
 #│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│#
 
@@ -34,9 +34,49 @@ def Part1(input):
     return sum(calibration_values)
 
 
+#╷----------.
+#│  Part 2  │
+#╵----------'
+def Part2(input):
+    INTS = [str(i) for i in range(10)]
+    WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    WORD_TO_INT = {k:v for (k, v) in zip(WORDS, INTS)}
+
+    calibration_values = []
+    
+    for line in input:
+        first_num = (len(line), '')   # (index, number)
+        last_num = (-1, '')           # (index, number)
+        
+        # Check for each int/word in the string and record its position (index of first character)
+        for num in (INTS + WORDS):
+            # First, check that number is found in line
+            if num in line:
+                
+                # Get first index from left
+                if (left := line.find(num)) < first_num[0]:
+                    first_num = (left, num)
+            
+                # Get first index from right
+                if (right := line.rfind(num)) > last_num[0]:
+                    last_num = (right, num)
+            
+            # If the current first/last numbers are both at the start/end of the line, end loop early
+            if (first_num[0] == 0) and (last_num[0] + len(last_num[1]) == len(line)):
+                break
+            
+        # Convert final digit values into integers and add combined number to list
+        first_digit = WORD_TO_INT[first_num[1]] if (first_num[1] in WORDS) else first_num[1]
+        last_digit = WORD_TO_INT[last_num[1]] if (last_num[1] in WORDS) else last_num[1]
+            
+        calibration_values.append(int(first_digit + last_digit))
+        
+    return sum(calibration_values)
+
+
 if __name__ == "__main__":
     input = Setup('day01_input.txt')
     
     print(f"[Part 1] Sum of calibration values: {Part1(input)}")
     
-    
+    print(f"[Part 2] Sum of corrected calibration values: {Part2(input)}")
