7923763ec6ef9f4020ff2f1cf714bd9694ad35b6
xram64 <33797815+xram64@users.noreply.github.com>
2023-12-01 20:19:01 -0500

    Day 1, Part 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Advent of Code 2023
+# 🎄 Advent of Code 2023 🎄
+
+## 📅 Day 1
+
+### Part 1
+```
+[Part 1] Sum of calibration values: 55002
+```
---
+++ day01/day01.py
@@ -0,0 +1,42 @@
+#│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│#
+#   ______________________________________________________________   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   |  <<  <<  <>  <|   Advent  of  Code  2023   |>  <>  >>  >>  |   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   | + ::|  Day  1  |:: + ::|  Jesse Williams  ∕  xram64  |:: + |   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   #
+#             __.-----------.___________________________             #
+#            |  |  Answers  |   Part 1: 55002           |            #
+#            |  `-----------'   Part 2:                 |            #
+#            `------------------------------------------'            #
+#│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│#
+
+
+def Setup(filename):
+    with open(filename) as f:
+        input = f.readlines()
+    return [line.strip() for line in input]
+
+
+#╷----------.
+#│  Part 1  │
+#╵----------'
+def Part1(input):
+    INTS = [str(i) for i in range(10)]
+    
+    calibration_values = []
+    
+    for line in input:
+        digits = [d for d in line if d in INTS]
+        calibration_values.append(int(digits[0] + digits[-1]))
+
+    return sum(calibration_values)
+
+
+if __name__ == "__main__":
+    input = Setup('day01_input.txt')
+    
+    print(f"[Part 1] Sum of calibration values: {Part1(input)}")
+    
+    
